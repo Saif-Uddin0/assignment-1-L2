0e1674f79c2625473b673c761bae6b95f28d76b1
Saif Uddin <mahirr.846@gmail.com>
2026-06-18 23:30:08 +0600

    added the the solution of number 6

--- solutions.ts
+++ solutions.ts
@@ -53,3 +53,62 @@ const user = {
 
 console.log(getProperty(user, "name"));
 console.log(getProperty(user, "age"));  
+
+
+
+// problem-5 
+
+interface Book {
+    title : string;
+    author: string;
+    publishedYear: number ;
+}
+
+function toggleReadStatus (book : Book){
+    return{
+        ...book,
+        isRead:true 
+    }
+};
+
+const myBook: Book = {
+  title: "TypeScript Guide",
+  author: "Jane Doe",
+  publishedYear: 2024,
+};
+
+console.log(toggleReadStatus(myBook));
+
+
+
+// problem -6 
+class Person{
+    name: string;
+    age: number;
+
+    constructor(name: string, age:number){
+        this.name = name;
+        this.age = age;
+    }
+
+}
+
+
+class Student extends Person{
+    grade: string;
+
+    constructor(name: string, age:number, grade: string){
+        super(name,age)
+        this.grade = grade;
+    }
+
+    getDetails(): string {
+    return `"Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}"`;
+  }
+
+}
+
+
+
+const student = new Student("Alice", 20, "A");
+console.log(student.getDetails());
